49a2582161bf6509e7eed19309a1f43d82268859
TechMel <125664209+TechMel@users.noreply.github.com>
2025-08-04 01:53:53 +0200

    helpPage.php

--- landing pages/helpPage.php
+++ landing pages/helpPage.php
@@ -6,11 +6,12 @@
     <title>How to Help - SPCA Grahamstown</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap">
+    <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="helpPage.css">
 </head>
 <body>
-    <?php
-    // include 'navbar functionalities/navbar.php';
+    <? php
+    include 'navbar.php';
     ?>
     
     <div class="container">
