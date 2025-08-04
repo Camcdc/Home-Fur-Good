3c3ebf5a89cf13815777b620fce463c3c92cf637
TechMel <125664209+TechMel@users.noreply.github.com>
2025-08-04 09:51:52 +0200

    helpPage.php

--- landing pages/helpPage.php
+++ landing pages/helpPage.php
@@ -7,10 +7,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap">
     <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="login-register.css">
     <link rel="stylesheet" href="helpPage.css">
 </head>
 <body>
-    <? php
+    <?php
     include 'navbar.php';
     ?>
     
